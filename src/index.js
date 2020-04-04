4347a3882657cbf6a1ae8a227dcd82a5922837fc
Manoj Naidu <manojnaidu619@gmail.com>
2020-04-04 15:31:19 +0530

    Done with tasks endpoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require("./db/mongoose")
 const User = require("./db/models/user")
+const Task = require("./db/models/task")
+
 const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,7 +11,17 @@ app.use(express.json())
 app.post("/users", (req, res) => {
     const data = req.body
     const user = new User(data)
-    user.save().then((result) => res.send(result)).catch((error) => res.send(error))
+    user.save()
+        .then((result) => res.send({message: "User Added", result: result._doc}))
+        .catch((error) => res.status(400).send({error: error.message}))
+})
+
+app.post("/tasks", (req, res) => {
+    const data = req.body
+    const task = new Task(data)
+    task.save()
+        .then((result) => res.send({message: "Task Added", result: result._doc}))
+        .catch((error) => res.status(400).send({error: error.message}))
 })
 
 app.listen(port, () => {
