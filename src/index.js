ac9dce93c7db8b3f07ae99a41e3f54487f5b38b4
Manoj Naidu <manojnaidu619@gmail.com>
2020-04-05 19:09:24 +0530

    user delete endpoint done

--- src/index.js
+++ src/index.js
@@ -101,6 +101,19 @@ app.patch("/tasks/:id", async(req, res) => {
     }
 })
 
+app.delete("/users/:id", async (req, res) => {
+    const id = req.params.id
+    try {
+        const user = await User.findByIdAndDelete(id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
 app.get("*", (req, res) => {
     res.send({error: "Invalid URI"})
 })
